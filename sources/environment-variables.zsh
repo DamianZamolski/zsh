e671444182454a4a30511faccec257adbb0d135b
Damian Zamolski <damian.zamola.zamolski@gmail.com>
2022-09-30 14:12:45 +0200

    Add history

--- sources/environment-variables.zsh
+++ sources/environment-variables.zsh
@@ -1,4 +1,7 @@
 export EDITOR=nvim
+export HISTFILE=~/.zsh_history
+export HISTSIZE=1000
 export NODE_EXTRA_CA_CERTS=~/c/certificates/npm-certificate
 export PATH=$PATH:~/dz/zsh/bin:~/.local/bin
+export SAVEHIST=1000
 export _JAVA_AWT_WM_NONREPARENTING=1 # Fix Jetbrains
