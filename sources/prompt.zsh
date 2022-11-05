cdefd8f8fb79f2229c769c1ee739849392f8e5e6
Damian Zamolski <damian.zamola.zamolski@gmail.com>
2022-11-05 17:53:28 +0100

    Make prompt dynamic

--- sources/prompt.zsh
+++ sources/prompt.zsh
@@ -3,10 +3,10 @@ setopt promptsubst
 autoload -Uz vcs_info
 precmd() { vcs_info }
 
-zstyle ':vcs_info:git:*' formats '@ %b'
+zstyle ':vcs_info:git:*' formats ' | %F{green}%b%f'
 
 PROMPT='
-%B[ %F{blue}%~%f | %F{green}${vcs_info_msg_0_}%f ]%b
+%B[ %F{blue}%~%f${vcs_info_msg_0_} ]%b
 '
 
 zle_highlight=(default:bold)
