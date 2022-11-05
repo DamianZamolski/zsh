5c87f37bb655d8f968c425343405458fff2d7dd6
Damian Zamolski <damian.zamola.zamolski@gmail.com>
2022-11-05 17:32:13 +0100

    Replace icons with characters in prompt

--- sources/prompt.zsh
+++ sources/prompt.zsh
@@ -3,9 +3,9 @@ setopt promptsubst
 autoload -Uz vcs_info
 precmd() { vcs_info }
 
-zstyle ':vcs_info:git:*' formats ' %b %u %c'
+zstyle ':vcs_info:git:*' formats '@ %b'
 
 PROMPT='
-%B%F{blue} %~%f   %F{green}${vcs_info_msg_0_}%f%b
+%B[ %F{blue}%~%f | %F{green}${vcs_info_msg_0_}%f ]%b
 '
 preexec() { echo }
