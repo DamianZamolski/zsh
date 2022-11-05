00156dcce1e3021aede5bd92fd07fe1dce93ef8f
Damian Zamolski <damian.zamola.zamolski@gmail.com>
2022-11-05 03:41:00 +0100

    Add > to prompt

--- sources/prompt.zsh
+++ sources/prompt.zsh
@@ -6,6 +6,6 @@ precmd() { vcs_info }
 zstyle ':vcs_info:git:*' formats '%b'
 
 PROMPT='
-%B%F{blue}%~%f %F{green}${vcs_info_msg_0_}%f%b
+%B> %F{blue}%~%f %F{green}${vcs_info_msg_0_}%f%b
 '
 preexec() { echo }
