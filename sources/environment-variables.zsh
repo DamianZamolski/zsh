c587c106472418a908e78af1207e42896273fd7d
Damian Zamolski <damian.zamola.zamolski@gmail.com>
2022-11-05 03:40:41 +0100

    Add browser and terminal environment variables

--- sources/environment-variables.zsh
+++ sources/environment-variables.zsh
@@ -1,6 +1,7 @@
-export BROWSER=firefox-developer-edition
+export BROWSER=firefox
 export EDITOR=nvim
 export HISTFILE=~/.zsh_history
 export HISTSIZE=1000
 export PATH=$PATH:~/dz/zsh/bin
 export SAVEHIST=1000
+export TERM=st
