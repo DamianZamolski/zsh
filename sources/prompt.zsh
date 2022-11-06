2de3e9322ab4bfd4b288eed3ce45d9e141409585
Damian Zamolski <damian.zamola.zamolski@gmail.com>
2022-11-06 18:31:28 +0100

    Remove empty lines between commands

--- sources/prompt.zsh
+++ sources/prompt.zsh
@@ -1,13 +1,9 @@
-setopt promptsubst
+setopt PROMPT_SUBST
 
 autoload -Uz vcs_info
 precmd() { vcs_info }
-
 zstyle ':vcs_info:git:*' formats ' | %F{green}%b%f'
 
-PROMPT='
-%B[ %F{blue}%~%f${vcs_info_msg_0_} ]%b
-'
+PROMPT='%B[ %F{blue}%~%f${vcs_info_msg_0_} ]%b '
 
 zle_highlight=(default:bold)
-preexec() { echo }
