b9f61c98fdbe4d56578e07ca8079224b616c485c
Damian Zamolski <damian.zamola.zamolski@gmail.com>
2022-11-06 18:33:01 +0100

    Change bin path

--- sources/environment-variables.zsh
+++ sources/environment-variables.zsh
@@ -2,6 +2,6 @@ export BROWSER=firefox
 export EDITOR=nvim
 export HISTFILE=~/.zsh_history
 export HISTSIZE=1000
-export PATH=$PATH:~/dz/zsh/bin
+export PATH=$PATH:~/dz/bin
 export SAVEHIST=1000
 export TERM=st
