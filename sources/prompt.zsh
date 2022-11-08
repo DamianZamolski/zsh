48b702f9ba968c763098e0ffe1b67da2ebc2ffc1
Damian Zamolski <damian.zamola.zamolski@gmail.com>
2022-11-08 08:06:57 +0100

    Add empty lines to prompt

--- sources/prompt.zsh
+++ sources/prompt.zsh
@@ -4,6 +4,9 @@ autoload -Uz vcs_info
 precmd() { vcs_info }
 zstyle ':vcs_info:git:*' formats ' | %F{green}%b%f'
 
-PROMPT='%B[ %F{blue}%~%f${vcs_info_msg_0_} ]%b '
+PROMPT='
+%B[ %F{blue}%~%f${vcs_info_msg_0_} ]%b
+'
 
 zle_highlight=(default:bold)
+preexec() { echo }
