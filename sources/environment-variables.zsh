d203426973dd5fd57a7459b1a8b16356dc5c1505
Damian Zamolski <damian.zamola.zamolski@gmail.com>
2023-02-06 11:30:05 +0100

    Update

--- sources/environment-variables.zsh
+++ sources/environment-variables.zsh
@@ -1,7 +1,7 @@
 export BROWSER=firefox
 export EDITOR=nvim
 export HISTFILE=~/.zsh_history
-export HISTSIZE=1000
+export HISTSIZE=1000000
 export PATH=$PATH:~/dz/bin
-export SAVEHIST=1000
+export SAVEHIST=1000000
 export TERM=st
--- sources/prompt.zsh
+++ sources/prompt.zsh
@@ -5,8 +5,8 @@ precmd() { vcs_info }
 zstyle ':vcs_info:git:*' formats ' | %F{green}%b%f'
 
 PROMPT='
-%B[ %F{blue}%~%f${vcs_info_msg_0_} ]%b
-'
+%B[ %F{blue}%~%f${vcs_info_msg_0_} ]
+>%b '
 
 zle_highlight=(default:bold)
 preexec() { echo }
